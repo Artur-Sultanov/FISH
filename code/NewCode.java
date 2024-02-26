27c040dbca945f5a66fec2e33cc9aeb5e764f6f6
Artur-Sultanov <arturo.sultanov@gmail.com>
2024-02-26 19:41:42 +0100

    algunos cambios

---
+++ code/NewCode.java
@@ -0,0 +1,11 @@
+public class NewCode {
+
+    public static int multiply (int a,int b){
+        return a*b;
+    }
+    
+    public static void main (String[] args){
+        System.out.println("Hola Mundo");
+        System.out.println(multiply(2,2));
+    }
+}
