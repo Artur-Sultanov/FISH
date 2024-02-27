772a6608180112578ec734266cb031f36af38876
Artur-Sultanov <arturo.sultanov@gmail.com>
2024-02-27 19:38:26 +0100

    o

--- code/supercode.java
+++ code/supercode.java
@@ -1,10 +1,8 @@
-public class NewCode {
-
+public class supercode {
     public static int multiply (int a,int b){
         return a*b;
     }
-    
-    public static void main (String[] args){
+        public static void main (String[] args){
         System.out.println("Hola Mundo");
         System.out.println(multiply(2,2));
     }
