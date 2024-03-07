8b4ea1a9bc3be86e3c1d3e00ad87255255499cff
Artur-Sultanov <arturo.sultanov@gmail.com>
2024-03-07 19:40:50 +0100

    esta creado nuevo issue #9

--- code/AnotherCode.java
+++
@@ -1,7 +0,0 @@
-public class AnotherCode {
-    public static void main(String[] args) {
-        System.out.println("another code");
-        System.out.println("nuevo cambio");
-    }
-    
-}
--- code/supercode.java
+++
@@ -1,9 +0,0 @@
-public class supercode {
-    public static int multiply (int a,int b){
-        return a*b;
-    }
-        public static void main (String[] args){
-        System.out.println("Hola Mundo");
-        System.out.println(multiply(2,2));
-    }
-}
